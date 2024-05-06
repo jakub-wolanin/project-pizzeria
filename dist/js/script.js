5c21fcb00191eb81ad4f5b9ef210a2671aac00a8
Jakub Wolanin <wolanin.kontakt@gmail.com>
2024-05-06 17:19:28 +0200

    add implementation of price calculation

--- dist/js/script.js
+++ dist/js/script.js
@@ -156,10 +156,27 @@
 
         // for every option in this category
         for (let optionId in param.options) {
-          
+
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
+
+          //check if there is param with a name of paramId in formData and if it includes optionId //
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+
+            //check if the option is not default//
+            if (!option.default) {
+
+              // add option price to price variable //
+              price += option.price;
+            }
+          } else {
+            // check if the option is default //
+            if (option.default) {
+              // reduce price variable
+              price -= option.price
+            }
+          }
         }
       }
 
