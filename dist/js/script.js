cefdb635ea1f105ae9d6588f278c775b8cae9165
Jakub Wolanin <wolanin.kontakt@gmail.com>
2024-05-09 17:45:42 +0200

    prepare function setValue

--- dist/js/script.js
+++ dist/js/script.js
@@ -223,6 +223,7 @@
       const thisWidget = this;
 
       thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
 
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
@@ -235,6 +236,24 @@
       thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
       thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
+    setValue(value) {
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+
+      // TODO: Add validation //
+      if (thisWidget.value !== newValue) {
+        thisWidget.value = newValue;
+      }
+
+      if (thisWidget.value !== newValue && !isNaN(newValue)) {
+        thisWidget.value = newValue;
+      }
+
+      thisWidget.value = newValue;
+      thisWidget.input.value = thisWidget.value;
     }
   }
 
